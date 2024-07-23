1af03f4c91209c77ec9a3ebbe040eb435c7fea85
Binyamin Klein <bendklein1@gmail.com>
2024-07-23 10:14:09 +0300

    Wrote more queries and logs

--- Integration_Queries.sql
+++ Integration_Queries.sql
@@ -1,5 +1,5 @@
 -- Queries for view 1
--- Select query to get the top 5 most profitable codeshare flights
+-- Query 1: Select query to get the top 5 most profitable codeshare flights
 SELECT 
     CodeShareID, MarketingAirline, FlightNumber, FlightDate, Origin, Destination, TotalRevenue
 FROM 
@@ -8,22 +8,22 @@ ORDER BY
     TotalRevenue DESC
 LIMIT 5;
 
--- Update query to adjust the marketing airline for a specific codeshare flight:
+-- Query 2: Update query to adjust the marketing airline for a specific codeshare flight:
 UPDATE CodeShareRevenue
 SET MarketingAirline = 'New Airline Partner'
 WHERE CodeShareID = 8 AND FlightNumber = 4;
 
--- Insert query to add a new codeshare flight:
+-- Query 3: Insert query to add a new codeshare flight:
 INSERT INTO CodeShareRevenue (CodeShareID, MarketingAirline, FlightNumber, FlightDate, FlightCode, Origin, Destination, TicketsSold, TotalRevenue)
 VALUES (123, 'Partner Airlines', 243234, '2024-08-16', 'CH1704', 'JFK', 'TLV', 178, 18000);
 
--- Delete the record with codeshare id 3895
+-- Query 4: Delete the record with codeshare id 3895
 DELETE FROM CodeShareRevenue
 WHERE CodeShareID = 3895;
 
 
 -- Queries for view 2
--- Select query to retrieve Current Capacity and Status for All Flights
+-- Query 5: Select query to retrieve Current Capacity and Status for All Flights
 SELECT
 	bookingID,
     flightNumber, 
@@ -33,21 +33,21 @@ SELECT
 FROM 
     FlightCapacityInfo;
 
--- Update query to Change the Capacity and Status of an Existing Flight
+-- Query 6: Update query to Change the Capacity and Status of an Existing Flight
 UPDATE flightCapacityInfo
 SET capacity = 180, status = 'Cancelled'
 WHERE flightNumber = 147 and DepartureDate = '2024-12-11';
 
--- Insert query to Add a New Flight and Its Capacity
+-- Query 7: Insert query to Add a New Flight and Its Capacity
 INSERT INTO flightCapacityInfo (flightNumber, departureDate, capacity, status, bookingID, price)
 VALUES (23534, '2024-08-01', 201, 'Booked', 83811, 360);
 
--- Delete a Flight from the Schedule
+-- Query 8: Delete a Flight from the Schedule
 DELETE FROM flightCapacityInfo
 WHERE FlightNumber = 147;
 
 
-
+-- Query 9
 -- Extra Select query for View 1: Calculate the average revenue per ticket for each marketing airline
 SELECT 
     MarketingAirline, 
@@ -59,8 +59,13 @@ GROUP BY
 ORDER BY 
     AvgRevenuePerTicket DESC;
 
+-- Query 10
+-- Extra query for View 1: find the total revenue for the year 2024:
+SELECT SUM(TotalRevenue) AS TotalRevenueInRange
+FROM CodeShareRevenue
+WHERE FlightDate BETWEEN '2024-01-01' AND '2024-12-31';
 
-
+-- Query 11
 -- Extra Select query for View 2: Retrieve Tickets for Flights with status of Booked
 SELECT 
     flightNumber, 
@@ -72,4 +77,17 @@ FROM
 WHERE 
     status = 'Booked';
 
+-- Query 12
+-- Extra Select query for View 2: Retrieve Flights with Bookings Above $800
+SELECT 
+    flightNumber, 
+    departureDate, 
+    capacity, 
+    status, 
+    bookingID, 
+    price 
+FROM 
+    FlightCapacityInfo
+WHERE 
+    price > 800;
 
